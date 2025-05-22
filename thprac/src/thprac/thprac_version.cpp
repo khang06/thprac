c1796bd2eb31ba50a0fedfbb5436b7b53dbb3bcc
Khang <khang06@users.noreply.github.com>
2025-05-22 19:44:36 -0400

    Bump version

--- thprac/src/thprac/thprac_version.cpp
+++ thprac/src/thprac/thprac_version.cpp
@@ -1,9 +1,9 @@
 ﻿#include "thprac_version.h"
 
 namespace THPrac {
-const char* g_thpracVersionStr { "2.5.1.0" };
-const wchar_t* g_thpracVersionWcs { L"2.5.1.0" };
-const int g_thpracVersionInt[] { 2, 5, 1, 0 };
+const char* g_thpracVersionStr { "2.6.0.0" };
+const wchar_t* g_thpracVersionWcs { L"2.6.0.0" };
+const int g_thpracVersionInt[] { 2, 6, 0, 0 };
 
 
 const char* GetVersionStr()
